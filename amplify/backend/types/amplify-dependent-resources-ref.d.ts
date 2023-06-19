c92ab73656acdccd1fcbcc40fb75b5d65a94ec5e
Your Name <you@example.com>
2023-06-19 22:34:20 +0100

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "filestorage27e7972a7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3filestorage2": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import { Amplify, Storage } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+
+  useEffect(() => {
+    fetchUploadedFiles();
+  }, []);
+
+  const fetchUploadedFiles = async () => {
+    try {
+      const response = await Storage.list('', { pageSize : 'ALL' });
+      const files = response.results.map((file) => ({
+        key: file.key,
+        url: `https://s3filestorage2231957-dev.s3.amazonaws.com/public/${file.key}` // Replace 'bucketName' with your actual S3 bucket name
+      }));
+      setUploadedFiles(files);
+    } catch (error) {
+      console.error('Error fetching uploaded files:', error);
+      alert('Error fetching uploaded files. Please try again.');
+    }
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleFileUpload = async () => {
+    if (!selectedFile) {
+      alert('Please select a file.');
+      return;
+    }
+
+    try {
+      await Storage.put(selectedFile.name, selectedFile);
+      alert('File uploaded successfully.');
+      fetchUploadedFiles(); // Fetch the updated list of uploaded files
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      alert('Error uploading file. Please try again.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>File Uploader</h1>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleFileUpload}>Upload</button>
+      {uploadedFiles.length > 0 && (
+        <div>
+          <h2>Uploaded Files:</h2>
+          <div className="file-cards">
+            {uploadedFiles.map((file) => (
+              <div key={file.key} className="file-card">
+                <img src={file.url} alt={file.key} className="file-image" />
+                <p className="file-name">{file.key}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
